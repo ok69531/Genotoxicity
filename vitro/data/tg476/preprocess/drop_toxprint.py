3cd44d77441e786419b119bde16a75fccf19775b
조소영 <ok69531@gmail.com>
2024-11-12 18:33:35 +0900

    init

---
+++ vitro/data/tg476/preprocess/drop_toxprint.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+smiles_df = pd.read_excel('../tg476_wSMILES_ToxPrint_desalt.xlsx')
+smiles_df = smiles_df.drop(['DTXSID'], axis = 1)
+
+cols = list(smiles_df.columns)
+cols[:3] = ['CasRN', 'Chemical', 'SMILES']
+smiles_df.columns = cols
+
+
+cols_front = ['Chemical', 'CasRN', 'SMILES', 'consv', 'maj']
+smiles_df = smiles_df[[c for c in cols_front if c in smiles_df] + [c for c in smiles_df if c not in cols_front]]
+
+df = smiles_df[smiles_df.iloc[:, 6].notna()].reset_index(drop = True)
+
+print(df.consv.value_counts())
+print(df.consv.value_counts(normalize=True))
+
+print(df.maj.value_counts())
+print(df.maj.value_counts(normalize=True))
+
+df.to_excel('../tg476.xlsx', header = True, index = False) 
