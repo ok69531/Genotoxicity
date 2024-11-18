7a11e7397d662277ce0aa63facd06213d4d968cf
조소영 <ok69531@gmail.com>
2024-11-18 10:28:10 +0900

    add - print

--- ml/ml_main.py
+++ ml/ml_main.py
@@ -95,6 +95,8 @@ def main():
     save_path = os.path.join(save_path, f'tg{args.tg_num}_{args.model}.json')
     json.dump(best_result, open(save_path, 'w'))
     
+    logging.info('')
+    logging.info('Model: {}'.format(args.model))
     logging.info('param: {}'.format(param))
     logging.info('test f1: ${{{:.3f}}}_{{\\pm {:.3f}}}$'.format(np.mean(test_f1s) * 100, np.std(test_f1s) * 100))
     logging.info('test precision: ${{{:.3f}}}_{{\\pm {:.3f}}}$'.format(np.mean(test_precs) * 100, np.std(test_precs) * 100))
